769b54bf10b45fdefd248c99e05fd5ee8c2e41b6
DdsT <hfr.ddst@gmail.com>
2020-11-07 01:06:33 +0100

    Create hfrfiltreav.meta.js

---
+++ hfrfiltreav.meta.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        [HFR] Filtre A&V
+// @namespace   ddst.github.io
+// @version     1.0.0
+// @author      DdsT
+// @downloadURL https://ddst.github.io/HFR_Filtre_AV/hfrfiltreav.user.js
+// @updateURL   https://ddst.github.io/HFR_Filtre_AV/hfrfiltreav.meta.js
+// @supportURL  https://ddst.github.io/HFR_Filtre_AV/
+// @description Filtrer les sujets des catégories Achats & Ventes
+// @icon        https://www.hardware.fr/images_skin_2010/facebook/logo.png
+// @match       *://forum.hardware.fr/forum1.php*&cat=6*
+// @match       *://forum.hardware.fr/forum1f.php*&cat=6*
+// @match       *://forum.hardware.fr/forum1.php*&cat=5*&subcat=251*
+// @match       *://forum.hardware.fr/forum1f.php*&cat=5*&subcat=251*
+// @match       *://forum.hardware.fr/hfr/AchatsVentes*/liste_sujet-*.htm
+// @match       *://forum.hardware.fr/hfr/JeuxVideo/Achat-Ventes/liste_sujet-*.htm
+// @require     https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM.getValue
+// @grant       GM.setValue
+// ==/UserScript==
